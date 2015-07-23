d48b7a28be89965d50b14e2eadd060a0c56184da
Tom Leach <tom@gc.io>
2015-07-23 15:49:14 -0400

    Setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+    name="confluent_schema_registry_client",
+    version="1.0.0",
+    author="Tom Leach",
+    author_email="tom@gc.com",
+    description="A simple client Confluent's Avro Schema Registry",
+    license="MIT",
+    keywords="confluent schema registry client avro http rest",
+    url="http://github.com/gamechanger/confluent_schema_registry_client",
+    packages=[""],
+    long_description="",
+    install_requires=[],
+    tests_require=['nose']
+    )
