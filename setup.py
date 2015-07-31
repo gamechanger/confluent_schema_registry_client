910b662a15289cd7e249811d3b03f14d6404f685
Tom Leach <tom@gc.io>
2015-07-31 16:09:09 -0400

    Don't require the latest requests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="confluent_schema_registry_client",
-    version="1.0.0",
+    version="1.0.1",
     author="Tom Leach",
     author_email="tom@gc.com",
     description="A simple client Confluent's Avro Schema Registry",
@@ -10,6 +10,6 @@ setuptools.setup(
     keywords="confluent schema registry client avro http rest",
     url="http://github.com/gamechanger/confluent_schema_registry_client",
     packages=["confluent_schema_registry_client"],
-    install_requires=['requests>=2.7.0'],
+    install_requires=['requests>=2.3.0'],
     tests_require=['nose', 'requests_mock']
     )
